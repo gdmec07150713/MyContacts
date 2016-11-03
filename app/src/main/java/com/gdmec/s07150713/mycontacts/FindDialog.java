1483116bf386741faa44e66c0e06a6bf48ba20a9
gdmec07150713 <849993281@qq.com>
2016-11-03 15:56:20 +0800

    Initial commit

---
+++ app/src/main/java/com/gdmec/s07150713/mycontacts/FindDialog.java
@@ -0,0 +1,13 @@
+package com.gdmec.s07150713.mycontacts;
+
+/**
+ * Created by Administrator on 2016/11/3.
+ */
+public class FindDialog {
+    public FindDialog(MainActivity mainActivity) {
+
+    }
+
+    public static void show() {
+    }
+}
